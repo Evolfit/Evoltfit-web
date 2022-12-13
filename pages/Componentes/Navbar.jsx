b788c278d6fb84967ff76cc8ae557d9e40689167
José Daniel Vázquez Franco <89546725+JDaniVF@users.noreply.github.com>
2022-12-13 11:51:05 -0600

    EVO-129 Algoritmo y SE
    Se mejoró el algoritmo para el sistema experto y se creo el apartado correspondiente para el sistema experto

--- pages/Componentes/Navbar.jsx
+++ pages/Componentes/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRouter } from "next/router";
-import { useState, useEffect } from 'react';
+import { useState, useEffect} from 'react';
 import supabase from '/config/supabaseClient';
 import Head from 'next/head';
 
