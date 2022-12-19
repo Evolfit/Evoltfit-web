45a7ba85b6848e7ee5d6c56066ba7d6d7249437c
José Daniel Vázquez Franco <89546725+JDaniVF@users.noreply.github.com>
2022-12-19 13:56:26 -0600

    Mejoras SE
    Boton SE

--- pages/sistemaexperto.js
+++ pages/sistemaexperto.js
@@ -68,9 +68,13 @@ export default function Home() {
         <div className='si'>
         {/*<button className="btn btn-outline rounded-full btn-secondary btn-lg m-8" onClick={handlePrevious}>Anterior</button>*/}
         {showButton ? (
-        <button className="btn btn-outline rounded-full btn-secondary btn-lg m-8" onClick={handleNext}>Siguiente</button>
+        <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800" onClick={handleNext}>
+        <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        Siguiente
+        </span>
+        </button>
         ) : (
-        <button>Siguiente</button>
+        <button class="text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-sm px-5 py-2.5 text-center" disabled>Siguiente</button>
         )}
         
         </div>
