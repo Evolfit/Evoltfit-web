8fe6f0473936205d3c50bf7771a9474c26f74e9d
Carlos Alberto Conchas Montañez <carlos--negro@hotmail.com>
2022-12-30 18:59:16 -0600

    Modificaciones

--- pages/login.js
+++ pages/login.js
@@ -93,7 +93,6 @@ export default function Home() {
       <br />
 
       <main>
-      
         <section className="flex flex-col md:flex-row h-screen items-center">
           <div className="bg-indigo-600 hidden lg:block w-full md:w-1/2 xl:w-2/3 h-screen">
             <img
@@ -103,7 +102,7 @@ export default function Home() {
             />
           </div>
           
-          <div className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
+          <div className="xl:mb-16 bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
             <div className="w-full h-100">
               <h1 className="text-xl md:text-2xl font-bold leading-tight">
                 Ingresa a tu cuenta
--- pages/registro.js
+++ pages/registro.js
@@ -126,7 +126,7 @@ export default function Home() {
 
       <main>
         <section className="flex flex-col md:flex-row h-screen items-center">
-          <div className="bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
+          <div className="mb-20 bg-white w-full md:max-w-md lg:max-w-full md:mx-auto md:mx-0 md:w-1/2 xl:w-1/3 h-screen px-6 lg:px-16 xl:px-12 flex items-center justify-center">
             <div className="w-full h-100">
               <h1 className="text-xl md:text-2xl font-bold leading-tight">
                 Registro
