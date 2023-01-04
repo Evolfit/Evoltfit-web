9720e37de0d0835c3b3b294f64d9ff595f5e6180
Carlos Alberto Conchas Montañez <carlos--negro@hotmail.com>
2023-01-04 00:51:13 -0600

    Modificaciones

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -3,72 +3,7 @@ import React from "react";
 export default function nosotros() {
   return (
     <div>
-      <ul className="items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="vue-checkbox-list"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-            />
-            <label
-              htmlFor="vue-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Vue JS
-            </label>
-          </div>
-        </li>
-        <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="react-checkbox-list"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-            />
-            <label
-              htmlFor="react-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              React
-            </label>
-          </div>
-        </li>
-        <li className="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="angular-checkbox-list"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-            />
-            <label
-              htmlFor="angular-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Angular
-            </label>
-          </div>
-        </li>
-        <li className="w-full dark:border-gray-600">
-          <div className="flex items-center pl-3">
-            <input
-              id="laravel-checkbox-list"
-              type="checkbox"
-              value=""
-              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-            />
-            <label
-              htmlFor="laravel-checkbox-list"
-              className="w-full py-3 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Laravel
-            </label>
-          </div>
-        </li>
-      </ul>
+      
     </div>
   );
 }
