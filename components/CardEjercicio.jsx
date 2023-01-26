9b7bca28deaf4222c82bfd56f825a2a7a377e280
juan-uribe-p <a19310177@ceti.mx>
2023-01-25 19:22:18 -0600

    Detalles y ajustes
    Se modificó un poco el diseño temporal de editarRutina

--- components/CardEjercicio.jsx
+++ components/CardEjercicio.jsx
@@ -87,36 +87,42 @@ const CardEjercicio = ({ rutinaEjercicio, getEjerciciosRutina, index }) => {
             <div className='absolute text-3xl right-0'>
                 <ion-icon name="reorder-three-outline"></ion-icon>
             </div>
-            <p className="mb-2 mr-8 text-2xl font-bold tracking-tight text-gray-900 cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden">
+            <p className="mb-2 mr-8 text-xl font-bold tracking-tight text-gray-900 cursor-pointer whitespace-nowrap text-ellipsis overflow-hidden">
                 {(index+1) + ' - ' + ejercicio.nombre}
             </p>
             {ejercicio.nombre == 'Descanso' ? 
                 ''
                 :
-                <p className="mb-3 font-normal text-xl text-gray-700">{ejercicio.musculo_primario}</p>
+                <p className="mb-3 font-normal text-lg text-gray-700">{ejercicio.musculo_primario}</p>
             }
-            <span className="text-xl font-semibold">
-                {ejercicio.nombre == 'Descanso' ? 
-                'Minutos: '
-                :
-                'Sets: '
-                }
-            </span>
-            <input name="sets" id="sets" type="number" className="input input-secondary w-full text-xl font-normal mt-2 mb-4" value={formInput.sets || ""} onChange={handleOnInputChange}/>
-            <br/>
-            <span className="text-xl font-semibold">
-            {ejercicio.nombre == 'Descanso' ? 
-                'Segundos: '
-                :
-                'Reps: '
-                }
-            </span>
-            <input name="reps" id="reps" type="number" className="input input-secondary w-full text-xl font-normal mt-2 mb-4" value={formInput.reps || ""} onChange={handleOnInputChange}/>
-            <br/>
-            <div className="flex content-center justify-center">
-                <img src={ejercicio.img} alt="" className='w-1/2'/>
+            <div className="flex flex-row">
+                <div className="w-1/2">
+                    <span className="text-lg font-semibold">
+                        {ejercicio.nombre == 'Descanso' ? 
+                        'Minutos: '
+                        :
+                        'Sets: '
+                        }
+                    </span>
+                    <input name="sets" id="sets" type="number" className="input input-secondary w-full text-lg font-normal mt-2 mb-4" value={formInput.sets || ""} onChange={handleOnInputChange}/>
+                    <br/>
+                    <span className="text-lg font-semibold">
+                    {ejercicio.nombre == 'Descanso' ? 
+                        'Segundos: '
+                        :
+                        'Reps: '
+                        }
+                    </span>
+                    <input name="reps" id="reps" type="number" className="input input-secondary w-full text-lg font-normal mt-2 mb-4" value={formInput.reps || ""} onChange={handleOnInputChange}/>
+                </div>
+                <div className="flex flex w-1/2 items-center justify-center p-6">
+                    {ejercicio.nombre == 'Descanso' ? 
+                        ''
+                        :
+                        <img src={ejercicio.img} alt=""/>
+                    }
+                </div>
             </div>
-            
             <button onClick={eliminarEjercicio} className="inline-flex items-center my-2 mx-1 px-3 py-2 text-sm font-medium text-center text-white bg-red-500 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300">
                 Eliminar
                 <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
