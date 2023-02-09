579d37e0e772bb14d45c7217ab4a1553d99a639e
juan-uribe-p <a19310177@ceti.mx>
2023-02-09 10:58:47 -0600

    Push Vercel

--- pages/comenzarRutina.js
+++ pages/comenzarRutina.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useState, useEffect, Fragment } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "/components/Navbar";
 import Footer from "/components/Footer";
 import supabase from "../config/supabaseClient";
@@ -18,7 +18,6 @@ export default function ComenzarRutina() {
   const [pausaTiempo, setPausaTiempo] = useState(true);
   const [comenzarEntrenamiento, setComenzarEntrenamiento] = useState(false);
   const [ejerciciosRutina, setEjerciciosRutina] = useState([])
-  const [toggleSeleccionar, setToggleSeleccionar] = useState(false);
   const [ejercicioSeleccionado, setEjercicioSeleccionado] = useState(0)
 
   useEffect(() => {
@@ -208,100 +207,91 @@ export default function ComenzarRutina() {
         <div>          
           {
             rutina ? 
-            <Fragment>
-              <div className={"mx-auto mt-2 " + (toggleSeleccionar ? 'blur-sm' : '')}>
-                <div className="flex flex-col w-11/12 xl:w-9/12 mx-auto">
-                  <div>
-                    <button className="btn btn-ghost m-0 px-2 text-lg" onClick={() => {router.push('/rutinas')}}>
-                      <div className='text-3xl mt-auto'>
-                        <ion-icon name="arrow-back-outline"></ion-icon>
-                      </div>
-                      <span className="ml-2">{"Volver a Rutinas"}</span>
-                    </button>
-                    <br/>
-                    <div className="flex flex-row items-center justify-center">
-                      <h2 className="flex-auto text-3xl text-gray-900">{rutina.nombre}</h2>
-                      <div className="w-28">
-                        <span>Tiempo: </span>
-                        <Cronometro
-                          pausaTiempo={pausaTiempo}
-                          setTiempo={setTiempo}
-                          tiempo={tiempo}
-                        />
-                      </div>
+            <div className={"mx-auto mt-2 " + (toggleSeleccionar ? 'blur-sm' : '')}>
+              <div className="flex flex-col w-11/12 xl:w-9/12 mx-auto">
+                <div>
+                  <button className="btn btn-ghost m-0 px-2 text-lg" onClick={() => {router.push('/rutinas')}}>
+                    <div className='text-3xl mt-auto'>
+                      <ion-icon name="arrow-back-outline"></ion-icon>
                     </div>
-                    { ejerciciosRutina.length === 0 ? 
-                        ''
+                    <span className="ml-2">{"Volver a Rutinas"}</span>
+                  </button>
+                  <br/>
+                  <div className="flex flex-row items-center justify-center">
+                    <h2 className="flex-auto text-3xl text-gray-900">{rutina.nombre}</h2>
+                    <div className="w-28">
+                      <span>Tiempo: </span>
+                      <Cronometro
+                        pausaTiempo={pausaTiempo}
+                        setTiempo={setTiempo}
+                        tiempo={tiempo}
+                      />
+                    </div>
+                  </div>
+                  { ejerciciosRutina.length === 0 ? 
+                      ''
+                    :
+                      !pausaTiempo ? 
+                        <div className="my-12">
+                          <div className="flex flex-row my-2">
+                            <button 
+                            className="bg-white rounded-lg shadow-md my-2 p-4 hover:bg-gray-50 duration-75 active:bg-blue-50 active:p-3.5"
+                            onClick={ejercicioAnterior}
+                            >
+                              {'<'}
+                            </button>
+                              <CardEjercicioEntrenamiento
+                                key={ejerciciosRutina[ejercicioSeleccionado].id}
+                                ejercicio={ejerciciosRutina[ejercicioSeleccionado]}
+                                updateSet={updateSet}
+                                ejercicioSeleccionado={ejercicioSeleccionado}
+                                agregarSet={agregarSet}
+                              />
+                            <button 
+                            className="bg-white rounded-lg shadow-md my-2 p-4 hover:bg-gray-50 duration-75 active:bg-blue-50 active:p-3.5"
+                            onClick={ejercicioSiguiente}
+                            >
+                              {'>'}
+                            </button>
+                          </div>
+                          <div className='flex flex-col justify-center items-center lg:flex-row w-full'>
+                            <button onClick={() => {setPausaTiempo(!pausaTiempo)}} className="flex-auto btn text-white btn-secondary rounded-lg btn-md mx-1 my-1 w-full duration-75 lg:my-0 active:bg-blue-800">
+                              {
+                              pausaTiempo ?
+                              'Reanudar Entrenamiento'
+                              :
+                              'Pausar Entrenamiento'
+                              }
+                            </button>
+                            <button onClick={() => {}} className="flex-auto  btn text-white btn-success rounded-lg btn-md mx-1 my-1 w-full lg:my-0">Finalizar</button>
+                          </div>  
+                        </div>
                       :
-                        !pausaTiempo ? 
-                          <div className="my-12">
-                            <div className="flex flex-row my-2">
-                              <button 
-                              className="bg-white rounded-lg shadow-md my-2 p-4 hover:bg-gray-50 duration-75 active:bg-blue-50 active:p-3.5"
-                              onClick={ejercicioAnterior}
-                              >
-                                {'<'}
-                              </button>
-                                <CardEjercicioEntrenamiento
-                                  key={ejerciciosRutina[ejercicioSeleccionado].id}
-                                  ejercicio={ejerciciosRutina[ejercicioSeleccionado]}
-                                  updateSet={updateSet}
-                                  ejercicioSeleccionado={ejercicioSeleccionado}
-                                  agregarSet={agregarSet}
-                                />
-                              <button 
-                              className="bg-white rounded-lg shadow-md my-2 p-4 hover:bg-gray-50 duration-75 active:bg-blue-50 active:p-3.5"
-                              onClick={ejercicioSiguiente}
-                              >
-                                {'>'}
-                              </button>
-                            </div>
-                            <div className='flex flex-col justify-center items-center lg:flex-row w-full'>
-                              <button onClick={() => {setPausaTiempo(!pausaTiempo)}} className="flex-auto btn text-white btn-secondary rounded-lg btn-md mx-1 my-1 w-full duration-75 lg:my-0 active:bg-blue-800">
-                                {
-                                pausaTiempo ?
+                        <div className="flex items-center justify-center my-12">
+                          <button onClick={() => {
+                            setPausaTiempo(!pausaTiempo)
+                            setComenzarEntrenamiento(true)
+                            }} 
+                            className="flex-auto btn text-white btn-secondary rounded-lg btn-md mx-1 my-1 w-full duration-75 lg:my-0 active:bg-blue-800 text-2xl h-20"
+                          >
+                              {
+                              pausaTiempo ?
+                                comenzarEntrenamiento ?
                                 'Reanudar Entrenamiento'
                                 :
-                                'Pausar Entrenamiento'
-                                }
-                              </button>
-                              <button onClick={() => {}} className="flex-auto  btn text-white btn-success rounded-lg btn-md mx-1 my-1 w-full lg:my-0">Finalizar</button>
-                            </div>  
-                          </div>
-                        :
-                          <div className="flex items-center justify-center my-12">
-                            <button onClick={() => {
-                              setPausaTiempo(!pausaTiempo)
-                              setComenzarEntrenamiento(true)
-                              }} 
-                              className="flex-auto btn text-white btn-secondary rounded-lg btn-md mx-1 my-1 w-full duration-75 lg:my-0 active:bg-blue-800 text-2xl h-20"
-                            >
-                                {
-                                pausaTiempo ?
-                                  comenzarEntrenamiento ?
-                                  'Reanudar Entrenamiento'
-                                  :
-                                  'Comenzar Entrenamiento'
-                                :
                                 'Comenzar Entrenamiento'
-                                }
-                              </button>
-                          </div>
-                    }             
-                  </div>
+                              :
+                              'Comenzar Entrenamiento'
+                              }
+                            </button>
+                        </div>
+                  }             
                 </div>
-                <div className="flex flex-col items-center w-full">
-                  {/* Aqui se muestran las rutinas */}
-                </div>
-              </div> 
-              { 
-                toggleSeleccionar ? 
-                <SeleccionarEjercicio
-                agregarEjercicio={agregarEjercicio}
-                setToggleSeleccionar={setToggleSeleccionar}
-                /> 
-                : '' }
-            </Fragment>
+              </div>
+              <div className="flex flex-col items-center w-full">
+                {/* Aqui se muestran las rutinas */}
+              </div>
+            </div> 
             : 
             <div className="mt-12">
               <div className="loader mt-6"></div>
