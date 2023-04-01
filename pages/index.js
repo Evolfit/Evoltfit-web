511fc4d0234d66fb7b18b9e1e1e8fd97fc03a96d
Carlos Alberto Conchas Montañez <carlos--negro@hotmail.com>
2023-04-01 16:35:10 -0600

    EVO-131 Documentación ejercicios
    Añadidos ejercicios de espalda superior

--- pages/index.js
+++ pages/index.js
@@ -50,15 +50,20 @@ export default function Home() {
               // getFullYear() Regresa el año actual
               var year_Actual = today.getFullYear();
 
-              var fecha_Actual = `${year_Actual}-${"0" + month_Actual}-${"0" + day_Actual}`
+              var fecha_Actual = `${year_Actual}-${"0" + month_Actual}-${
+                "0" + day_Actual
+              }`;
 
               console.log("Fecha de hoy: " + fecha_Actual);
 
-              if ((fecha_Actual == sus_pagos[0].fecha_termino) || (sus_pagos[0].fecha_termino < fecha_Actual)) {
+              if (
+                fecha_Actual == sus_pagos[0].fecha_termino ||
+                sus_pagos[0].fecha_termino < fecha_Actual
+              ) {
                 console.log("Ya se acabó el plan");
                 let { data, error } = await supabase
                   .from("sus_pagos")
-                  .update({ activo: 0})
+                  .update({ activo: 0 })
                   .eq("id_usuario", sesion.user.id);
 
                 if (error) {
@@ -776,11 +781,12 @@ export default function Home() {
             </h2>
 
             <p className="mt-4 text-sm text-gray-500 lg:text-base">
-              EvoltFit esta preparado para ayudar a todo tipo de usuarios. Si eres 
-              principiante te proporcionamos la guía perfecta para obtener la mejor rutina para ti
-              y así aprendas lo que le funciona a tu cuerpo; en cambio si eres alguien con conocimientos 
-              previos ponemos a tu disposición herramientas para que puedas organizar tu entrenamiento 
-              de la mejor forma posible.
+              EvoltFit esta preparado para ayudar a todo tipo de usuarios. Si
+              eres principiante te proporcionamos la guía perfecta para obtener
+              la mejor rutina para ti y así aprendas lo que le funciona a tu
+              cuerpo; en cambio si eres alguien con conocimientos previos
+              ponemos a tu disposición herramientas para que puedas organizar tu
+              entrenamiento de la mejor forma posible.
             </p>
 
             <div className="flex flex-col mt-6 space-y-3 lg:space-y-0 lg:flex-row">
@@ -802,6 +808,11 @@ export default function Home() {
           </div>
         </div>
       </div>
+
+      <br></br>
+      
+      
+
       <br />
       <br />
       <br />
